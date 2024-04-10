22b1ea4c7b0a48ac3aa921f8c85063ed114ffa74
chinhph28756 <chinhddph28756@fpt.edu.vn>
2024-04-10 10:29:43 +0700

    bai-1

---
+++ bai1.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Write a PHP class 'Rectangle' that has properties for length and width. Implement methods to c the rectangle's area and perimeter.
+(source main: https://www.w3resource.com/php-exercises/oop/index.php)
+*/
+
+class Rectangle {
+
+    // properties
+    public $length = 5;
+    public $width = 10;
+
+    // methods
+    public function rectangle_perimeter() {
+        echo "Với chiều dài =".$this->length."</br>";
+        echo "và chiều rộng =".$this->width."</br>";
+        echo "Chu vi hình chữ nhật sẽ là: (".$this->length." + ".$this->width.") * 2 = ".($this->length + $this->width)*2;
+    }
+
+}
+
+$cn1 = new Rectangle;
+$cn1->length = 3;
+$cn1->width = 2;
+$cn1->rectangle_perimeter();
+?>
